0be36b645e371b48db278cb3c8e26667b048be6d
Rim Fardiyev <threeacesstl@gmail.com>
2018-08-05 12:25:48 -0500

    Search bug fixed

--- SportsWebApp/Controllers/BlogController.cs
+++ SportsWebApp/Controllers/BlogController.cs
@@ -1,8 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using ButterCMS;
+
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -10,10 +9,21 @@ namespace SportsWebApp.Controllers
 {
     public class BlogController : Controller
     {
-        // GET: /<controller>/
-        public IActionResult Index()
+        private static string _apiToken = "1cadd5d5c66eed9dbec3bf8a8a8df3c7b9f05e04";
+
+        private ButterCMSClient _client;
+
+        public BlogController ()
         {
-            return View();
+            _client = new ButterCMSClient(_apiToken);
+        }
+
+        [Route("Blog/nba-blog-post")]
+        public async Task<ActionResult> ShowPost(string slug)
+        {
+            var response = await _client.RetrievePostAsync(slug);
+            ViewBag.Post = response.Data;
+            return View("Post");
         }
     }
 }
